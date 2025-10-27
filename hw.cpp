502d1f8cc729b533c51c123ad459a19523543672
s1140363-star <s1140363@dmhs.kh.edu.tw>
2025-10-27 16:28:47 +0800

    Add files via upload

---
+++ hw.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    int age = 15;
+    double hight = 181;
+    std::string name = "zrz";
+    bool is_student = false;
+    std::cout << "姓名: " << name << std::endl;
+    std::cout << "身高: " << hight << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生: " << is_student << std::endl;
+    
+    return 0;
+}
